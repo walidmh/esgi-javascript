45e8290de41edaa6f195be40e4aa06e5af1f4919
walidmh <walid_mohsin10@hotmail.com>
2018-11-05 17:21:27 +0100

    Exercice 1 part1
    Signed-off-by: walidmh <walid_mohsin10@hotmail.com>

---
+++ exercice-1/string.js
@@ -0,0 +1,62 @@
+var mot = "";
+var chaine = "";
+var i = 0;
+
+function ucFirst(mot)
+{
+	return mot.substr(0,1).toUpperCase()+mot.substr(1,mot.length).toLowerCase();
+}
+
+function capitalize(chaine)
+{
+	
+ 
+    while (chaine[i] != '\0')
+    {
+        
+        if (chaine[i]  >= 97 &&  chaine[i] <= 122)
+        {
+            chaine[i] = chaine[i] - 32;
+        }
+    	i++;
+    }
+    
+	
+}
+
+function camelCase(chaine)
+{
+	
+ 
+    while (chaine[i] != '\0')
+    {
+        
+        if (chaine[i]  >= 97 &&  chaine[i] <= 122)
+        {
+            chaine[i] = chaine[i] - 32;
+        }
+        
+
+        else if(chaine[i] = ' ')
+        {
+        	chaine[i].replace(/ /g,"");
+        }
+    	i++;
+    }
+    
+	
+}
+
+
+function snake_case(chaine)
+{
+	for(i==0 ; i<=chaine.length ; i++)
+	{
+		chaine[i].replace(/ /g,"_");
+		chaine = chaine + chaine[i];
+	}
+	return chaine;
+}
+
+console.log(snake_case("walid est en classe"));
+
